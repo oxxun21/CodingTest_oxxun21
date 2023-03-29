c0cc56e47cae1cbfd9e58d6151793d49fcf25767
Oh Ga Eun <dhrkdms4113@naver.com>
2023-03-29 17:08:01 +0900

    23-03-29 짝수는 싫어요

---
+++ 짝수는싫어요.js
@@ -0,0 +1,15 @@
+// 정수 n이 매개변수로 주어질 때, n 이하의 홀수가 오름차순으로 담긴 배열을 return하도록 solution 함수를 완성해주세요.
+
+// 제한사항
+// 1 ≤ n ≤ 100
+
+function solution(n) {
+  let odd = [];
+
+  for(let i = 1; i <= n; i++){
+    if(i % 2 === 1) {
+      odd.push(i);
+    }
+  }
+  return odd;
+}
