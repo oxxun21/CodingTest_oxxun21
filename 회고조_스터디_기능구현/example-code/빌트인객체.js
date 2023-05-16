13bca867ca285ab946961bcb418c984eadfb726f
Oh Ga Eun <dhrkdms4113@naver.com>
2023-05-16 23:38:44 +0900

    21장 빌트인 객체 | 모던 자바스크립트 Deep Dive

---
+++ 회고조_스터디_기능구현/example-code/빌트인객체.js
@@ -0,0 +1,252 @@
+// 21-01
+// String 생성자 함수에 의한 String 객체 생성
+const strObj = new String('Lee'); // String {"Lee"}
+console.log(typeof strObj);       // object
+
+// Number 생성자 함수에 의한 Number 객체 생성
+const numObj = new Number(123); // Number {123}
+console.log(typeof numObj);     // object
+
+// Boolean 생성자 함수에 의한 Boolean 객체 생성
+const boolObj= new Boolean(true); // Boolean {true}
+console.log(typeof boolObj);      // object
+
+// Function 생성자 함수에 의한 Function 객체(함수) 생성
+const func = new Function('x', 'return x * x'); // ƒ anonymous(x )
+console.log(typeof func);                       // function
+
+// Array 생성자 함수에 의한 Array 객체(배열) 생성
+const arr = new Array(1, 2, 3); // (3) [1, 2, 3]
+console.log(typeof arr);        // object
+
+// RegExp 생성자 함수에 의한 RegExp 객체(정규 표현식) 생성
+const regExp = new RegExp(/ab+c/i); // /ab+c/i
+console.log(typeof regExp);         // object
+
+// Date 생성자 함수에 의한 Date 객체 생성
+const date = new Date();  // Fri May 08 2020 10:43:25 GMT+0900 (대한민국 표준시)
+console.log(typeof date); // object
+
+// 21-02
+// String 생성자 함수에 의한 String 객체 생성
+const strObj = new String('Lee');  // String {"Lee"}
+
+// String 생성자 함수를 통해 생성한 strObj 객체의 프로토타입은 String.prototype 이다.
+console.log(Object.getPrototypeOf(strObj) === String.prototype);  // true
+
+// 21-03
+// Number 생성자 함수에 의한 Number 객체 생성
+const numObj = new Number(1.5);   // Number {1.5}
+
+// toFixed는 Number.prototype의 프로토타입 메서드다.
+// Number.prototype.toFixed는 소수점 자리를 반올림하여 문자열로 반환한다.
+console.log(numObj.toFixed());
+
+// isInteger는 Number의 정적 메서드다.
+// Number.isInteger는 인수가 정수인지 검사하여 그 결과를 Boolean으로 반환한다.
+console.log(Number.isInteger(0.5)); // false
+
+// 21-04
+const str = 'hello';
+
+// 원시 타입인 문자열이 프로퍼티와 메서드를 갖고 있는 객체처럼 동작한다.
+console.log(str.length);  // 5
+console.log(str.toUpperCase);   // HELLO
+
+// 21-05
+const str = 'hi';
+
+// 원시 타입인 문자열이 래퍼 객체인 String 인스턴스로 변환된다.
+console.log(str.length);  // 2
+console.log(str.toUpperCase);   // HI
+
+// 래퍼 객체로 프로퍼티에 접근하거나 메서드를 호출한 후, 다시 원시값으로 되돌린다.
+console.log(typeof str);  // string
+
+// 21-06
+// 1. 식별자 str은 문자열을 값으로 가지고 있다.
+const str = 'hello';
+
+// 2. 식별자 str은 암묵적으로 생성된 래퍼 객체를 가리킨다.
+// 식별자 str의 값 'hello'는 래퍼 객체의 [[StringData]] 내부 슬롯에 할당된다.
+// 래퍼 객체에 name 프로퍼티가 동적 추가된다.
+str.name = 'Lee';
+
+// 3. 식별자 str은 다시 원래의 문자열, 즉 래퍼 객체의 [[StringData]] 내부 슬롯에 할당된 원시값을 갖는다.
+// 이때 2에서 생성된 래퍼 객체는 아무도 참조하지 않는 상태이므로 가비지 컬렉션의 대상이 된다.
+
+// 4. 식별자 str은 새롭게 암묵적으로 생성된(2에서 생성된 래퍼 객체와는 다른) 래퍼 객체를 가리킨다.
+// 새롭게 생성된 래퍼 객체에는 name 프로퍼티가 존재하지 않는다.
+console.log(str.name);  // undefined 
+
+// 5. 식별자 str은 다시 원래의 문자열, 즉 래퍼 객체의 [[StringData]] 내부 슬롯에 할당된 원시값을 갖는다.
+// 이때 4에서 생성된 래퍼 객체는 아무도 참조하지 않는 상태이므로 가비지 컬렉션의 대상이 된다.
+console.log(typeof str, str); // string hello
+
+// 21-07
+const num = 1.5;
+
+// 원시 타입인 숫자가 래퍼 객체인 Number 객체로 변환된다.
+console.log(num.toFixed());   // 2
+
+// 래퍼 객체로 프로퍼티에 접근하거나 메서드를 호출한 후, 다시 원시값으로 되돌린다.
+console.log(typeof num, num);   // number 1.5
+
+// 21-20
+// 인수가 유한수이면 true를 반환한다.
+isFinite(0);    // -> true
+isFinite(2e64); // -> true
+isFinite('10'); // -> true: '10' → 10
+isFinite(null); // -> true: null → 0
+
+// 인수가 무한수 또는 NaN으로 평가되는 값이라면 false를 반환한다.
+isFinite(Infinity);  // -> false
+isFinite(-Infinity); // -> false
+
+// 인수가 NaN으로 평가되는 값이라면 false를 반환한다.
+isFinite(NaN);     // -> false
+isFinite('Hello'); // -> false
+isFinite('2005/12/12'); // -> false
+
+// 21-22
+// 숫자
+isNaN(NaN); // -> true
+isNaN(10);  // -> false
+
+// 문자열
+isNaN('blabla'); // -> true: 'blabla' => NaN
+isNaN('10');     // -> false: '10' => 10
+isNaN('10.12');  // -> false: '10.12' => 10.12
+isNaN('');       // -> false: '' => 0
+isNaN(' ');      // -> false: ' ' => 0
+
+// 불리언
+isNaN(true); // -> false: true → 1
+isNaN(null); // -> false: null → 0
+
+// undefined
+isNaN(undefined); // -> true: undefined => NaN
+
+// 객체
+isNaN({}); // -> true: {} => NaN
+
+// date
+isNaN(new Date());            // -> false: new Date() => Number
+isNaN(new Date().toString()); // -> true:  String => NaN
+
+// 21-23
+// 문자열을 실수로 해석하여 반환한다.
+parseFloat('3.14');  // -> 3.14
+parseFloat('10.00'); // -> 10
+
+// 공백으로 구분된 문자열은 첫 번째 문자열만 변환한다.
+parseFloat('34 45 66'); // -> 34
+parseFloat('40 years'); // -> 40
+
+// 첫 번째 문자열을 숫자로 변환할 수 없다면 NaN을 반환한다.
+parseFloat('He was 40'); // -> NaN
+
+// 앞뒤 공백은 무시된다.
+parseFloat(' 60 '); // -> 60
+
+// 21-24
+// 문자열을 정수로 해석하여 반환한다.
+parseInt('10');     // -> 10
+parseInt('10.123'); // -> 10
+
+parseInt(10);     // -> 10
+parseInt(10.123); // -> 10
+
+// 10'을 10진수로 해석하고 그 결과를 10진수 정수로 반환한다
+parseInt('10'); // -> 10
+// '10'을 2진수로 해석하고 그 결과를 10진수 정수로 반환한다
+parseInt('10', 2); // -> 2
+// '10'을 8진수로 해석하고 그 결과를 10진수 정수로 반환한다
+parseInt('10', 8); // -> 8
+// '10'을 16진수로 해석하고 그 결과를 10진수 정수로 반환한다
+parseInt('10', 16); // -> 16
+
+const x = 15;
+
+// 10진수 15를 2진수로 변환하여 그 결과를 문자열로 반환한다.
+x.toString(2); // -> '1111'
+// 문자열 '1111'을 2진수로 해석하고 그 결과를 10진수 정수로 반환한다
+parseInt(x.toString(2), 2); // -> 15
+
+// 10진수 15를 8진수로 변환하여 그 결과를 문자열로 반환한다.
+x.toString(8); // -> '17'
+// 문자열 '17'을 8진수로 해석하고 그 결과를 10진수 정수로 반환한다
+parseInt(x.toString(8), 8); // -> 15
+
+// 10진수 15를 16진수로 변환하여 그 결과를 문자열로 반환한다.
+x.toString(16); // -> 'f'
+// 문자열 'f'를 16진수로 해석하고 그 결과를 10진수 정수로 반환한다
+parseInt(x.toString(8), 8); // -> 15
+
+// 숫자값을 문자열로 변환한다.
+x.toString(); // -> '15'
+// 문자열 '15'를 10진수로 해석하고 그 결과를 10진수 정수로 반환한다
+parseInt(x.toString()); // -> 15
+
+// 16진수 리터럴 '0xf'를 16진수로 해석하고 10진수 정수로 그 결과를 반환한다.
+parseInt('0xf'); // -> 15
+// 위 코드와 같다.
+parseInt('f', 16); // -> 15
+
+// 공백으로 구분된 문자열은 첫 번째 문자열만 변환한다.
+parseInt('34 45 66'); // -> 34
+parseInt('40 years'); // -> 40
+// 첫 번째 문자열을 숫자로 변환할 수 없다면 NaN을 반환한다.
+parseInt('He was 40'); // -> NaN
+// 앞뒤 공백은 무시된다.
+parseInt(' 60 '); // -> 60
+
+// 21-37
+var x = 10; // 전역 변수
+
+function foo(){
+  // 선언하지 않은 식별자에 값을 할당
+  y = 20; // window.y = 20;
+}
+foo();
+
+// 선언하지 않은 식별자 y를 전역에서 참조할 수 있다.
+console.log(x + y);  // 30
+
+// 21-38
+// 전역 변수 x는 호이스팅이 발생한다.
+console.log(x); // undefined
+
+// 전역 변수가 아니라 단지 전역 객체의 프로퍼티인 y는 호이스팅이 발생하지 않는다.
+console.log(y); // ReferenceError: y is not defined
+
+var x = 10; // 전역 변수
+
+function foo () {
+  // 선언하지 않은 식별자에 값을 할당
+  y = 20; // window.y = 20;
+}
+foo();
+
+// 선언하지 않은 식별자 y를 전역에서 참조할 수 있다.
+console.log(x + y); // 30
+
+// 21-39
+var x = 10; // 전역 변수
+
+function foo () {
+  // 선언하지 않은 식별자에 값을 할당
+  y = 20; // window.y = 20;
+  console.log(x + y);
+}
+
+foo(); // 30
+
+console.log(window.x); // 10
+console.log(window.y); // 20
+
+delete x; // 전역 변수는 삭제되지 않는다.
+delete y; // 프로퍼티는 삭제된다.
+
+console.log(window.x); // 10
+console.log(window.y); // undefined
