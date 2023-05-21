33cdc0df220d0cc46a61feb5be93c6d5d68d89c6
Oh Ga Eun <dhrkdms4113@naver.com>
2023-05-21 15:46:38 +0900

    예제 업데이트

--- 회고조_스터디_기능구현/reactvlpt/examplefirst/src/App.js
+++ 회고조_스터디_기능구현/reactvlpt/examplefirst/src/App.js
@@ -1,22 +1,25 @@
-import { useRef, useState } from "react";
+import { useCallback, useMemo, useRef, useState } from "react";
 import CreateUser from "./components/CreateUser";
 import UserList from "./components/UserList";
 
-function App(){
+function countActiveUsers(users) {
+  console.log('활성 사용자 수를 세는중...');
+  return users.filter(user => user.active).length;
+}
+
+function App() {
   const [inputs, setInputs] = useState({
     username: '',
-    email: '',
-  })
-
-  const {username, email} = inputs;
-
-  const onChange = (e) => {
-    const {name, value} = e.target;
-    setInputs({
+    email: ''
+  });
+  const { username, email } = inputs;
+  const onChange = useCallback(e => {
+    const { name, value } = e.target;
+    setInputs(inputs => ({
       ...inputs,
-      [name] : value
-    })
-  }
+      [name]: value
+    }));
+  }, []);
 
   const [users, setUsers] = useState([
     {
@@ -40,42 +43,47 @@ function App(){
   ]);
 
   const nextId = useRef(4);
-
-  const onCreate = () => {
+  const onCreate = useCallback(() => {
     const user = {
       id: nextId.current,
       username,
-      email,
-    }
-    setUsers(users.concat(user));
+      email
+    };
+    setUsers(users => users.concat(user));
 
     setInputs({
       username: '',
-      email: '',
-    })
+      email: ''
+    });
     nextId.current += 1;
-  }
+  }, [username, email]);
 
-  const onRemove = id => {
-    // user.id 가 파라미터로 일치하지 않는 원소만 추출하여 새로운 배열 생성
-    // = user.id가 id인 것을 제거
-    setUsers(users.filter(user => user.id !== id));
-  }
+  const onRemove = useCallback(id => {
+    // user.id 가 파라미터로 일치하지 않는 원소만 추출해서 새로운 배열을 만듬
+    // = user.id 가 id 인 것을 제거함
+    setUsers(users => users.filter(user => user.id !== id));
+  }, []);
 
-  const onToggle = id => {
-    setUsers(
-      users.map(user => 
+  const onToggle = useCallback(id => {
+    setUsers(users =>
+      users.map(user =>
         user.id === id ? { ...user, active: !user.active } : user
       )
-    )
-  }
-
+    );
+  }, []);
+  const count = useMemo(() => countActiveUsers(users), [users]);
   return (
     <>
-      <CreateUser username={username} email={email} onChange={onChange} onCreate={onCreate} />
+      <CreateUser
+        username={username}
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate}
+      />
       <UserList users={users} onRemove={onRemove} onToggle={onToggle} />
+      <div>활성사용자 수 : {count}</div>
     </>
-  )
+  );
 }
 
 export default App;
--- 회고조_스터디_기능구현/reactvlpt/examplefirst/src/components/UserList.jsx
+++ 회고조_스터디_기능구현/reactvlpt/examplefirst/src/components/UserList.jsx
@@ -1,13 +1,37 @@
-import User from "./User"
+import React from 'react';
 
-export default function UserList({users, onRemove, onToggle}){
+const User = React.memo(function User({ user, onRemove, onToggle }) {
   return (
     <div>
-      {users.map(user => {
-        return (
-          <User user={user} key={user.id} onRemove={onRemove} onToggle={onToggle} />
-        )
-      })}
+      <b
+        style={{
+          cursor: 'pointer',
+          color: user.active ? 'green' : 'black'
+        }}
+        onClick={() => onToggle(user.id)}
+      >
+        {user.username}
+      </b>
+      &nbsp;
+      <span>({user.email})</span>
+      <button onClick={() => onRemove(user.id)}>삭제</button>
     </div>
-  )
+  );
+});
+
+function UserList({ users, onRemove, onToggle }) {
+  return (
+    <div>
+      {users.map(user => (
+        <User
+          user={user}
+          key={user.id}
+          onRemove={onRemove}
+          onToggle={onToggle}
+        />
+      ))}
+    </div>
+  );
 }
+
+export default React.memo(UserList);
