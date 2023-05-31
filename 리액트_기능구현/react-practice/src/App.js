ca628b36aea5c9032992c803bf25aba3966a8302
Oh Ga Eun <dhrkdms4113@naver.com>
2023-05-31 22:52:21 +0900

    리액트로 Blog 구현하기 초기 세팅

---
+++ 리액트_기능구현/react-practice/src/App.js
@@ -0,0 +1,15 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import BlogDetail from './pages/BlogDetail/BlogDetail';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/blog/:id" element={<BlogDetail />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+export default App;
