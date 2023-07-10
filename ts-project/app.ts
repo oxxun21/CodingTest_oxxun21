cd2aa2de502a492b1028b78a36e3c58094747f7a
Oh Ga Eun <dhrkdms4113@naver.com>
2023-07-11 00:02:00 +0900

    기본 타입 (1)

--- ts-project/app.ts
+++ ts-project/app.ts
@@ -1,1 +1,26 @@
-console.log('Your code goes here...');
+// 유니언, 리터럴
+function combine(
+  input1: string | number,
+  input2: string | number,
+  resultConversion: "as-number" | "as-text"
+) {
+  let result;
+  if (
+    (typeof input1 === "number" && typeof input2 === "number") ||
+    resultConversion === "as-number"
+  ) {
+    result = +input1 + +input2;
+  } else {
+    result = input1.toString() + input2.toString();
+  }
+  return result;
+}
+
+const combinedAges = combine(20, 26, "as-number");
+console.log(combinedAges);
+
+const combinedStringAges = combine("20", "26", "as-number");
+console.log(combinedAges);
+
+const combinedNames = combine("max", "anna", "as-text");
+console.log(combinedNames);
