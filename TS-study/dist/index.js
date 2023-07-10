d423589076954911e2159c499c656f1023359041
Oh Ga Eun <dhrkdms4113@naver.com>
2023-07-10 17:19:16 +0900

    TypeScript 기본 세팅

--- TS-study/dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-let x = 3;
-let y = 4;
-console.log(x + y);
-console.log(x + y);
-console.log(x + y);
--- TS-study/src/index.ts
+++
@@ -1,5 +0,0 @@
-let x: number = 3;
-let y = 4;
-console.log(x + y);
-console.log(x + y);
-console.log(x + y);
