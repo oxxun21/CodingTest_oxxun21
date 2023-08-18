2bf09054591ea6c779f93ec9c6db3d1822257b4f
Oh Ga Eun <dhrkdms4113@naver.com>
2023-08-18 17:42:54 +0900

    2023-08-18 프로그래머스 PS

---
+++ 코딩테스트_문제풀이/Lv0/최댓값만들기(2).js
@@ -0,0 +1,14 @@
+// 문제 설명
+// 정수 배열 numbers가 매개변수로 주어집니다. numbers의 원소 중 두 개를 곱해 만들 수 있는 최댓값을 return하도록 solution 함수를 완성해주세요.
+
+// 제한사항
+// -10,000 ≤ numbers의 원소 ≤ 10,000
+// 2 ≤ numbers 의 길이 ≤ 100
+
+function solution(numbers) {
+  numbers.sort((a, b) => b - a);
+  if (numbers[0] * numbers[1] < numbers.slice(-2)[0] * numbers.slice(-2)[1]) {
+    return numbers.slice(-2)[0] * numbers.slice(-2)[1];
+  }
+  return numbers[0] * numbers[1];
+}
