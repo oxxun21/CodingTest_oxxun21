104781c070c2ad822350f866d3ea39a2114a0894
Ga-eun <98699927+oxxun21@users.noreply.github.com>
2023-08-18 16:47:35 +0900

    Update A＋B － 3.js

--- 백준/Bronze/10950. A＋B － 3/A＋B － 3.js
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.js
@@ -1,8 +1,6 @@
 const fs = require("fs")
 let input = fs.readFileSync("/dev/stdin", "utf8").toString().split("\n");
 
-const arr = input.map((i) => i.split(" ").map(Number));
-
 for (let i = 1; i <= input[0]; i++) {
   let arr = input[i].split(" ").map(Number)
   console.log(arr[0] + arr[1]);
