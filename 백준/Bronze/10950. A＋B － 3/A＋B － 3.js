f54aba3b1cab23b3b5697c36de7c4cb7db85ee47
Oh Ga Eun <dhrkdms4113@naver.com>
2023-08-18 16:55:29 +0900

    remove

--- 백준/Bronze/10950. A＋B － 3/A＋B － 3.js
+++
@@ -1,7 +0,0 @@
-const fs = require("fs")
-let input = fs.readFileSync("/dev/stdin", "utf8").toString().split("\n");
-
-for (let i = 1; i <= input[0]; i++) {
-  let arr = input[i].split(" ").map(Number)
-  console.log(arr[0] + arr[1]);
-}
--- 백준/Bronze/2558. A＋B － 2/A＋B － 2.js
+++
@@ -1,4 +0,0 @@
-const filePath = process.platform === "linux" ? "/dev/stdin" : "./test.txt";
-let input = require("fs").readFileSync(filePath).toString().trim().split("\n");
-
-console.log(parseInt(input[0]) + parseInt(input[1]));
